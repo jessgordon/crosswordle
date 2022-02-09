771972be5b8929f72f87b2bd0bb06ffad0b69ad3
bryn9696 <bryn2009@live.co.uk>
2022-02-09 14:55:45 +0000

    written a feature test for the score

---
+++ myapp/src/FeatureTests/Score.test.js
@@ -0,0 +1,5 @@
+
+
+it("increases score by one", () => {
+  expect()
+} )
---
+++ myapp/src/Score.js
@@ -0,0 +1,10 @@
+
+
+import React from 'react'
+
+export default function Score() {
+  return (
+    <div>Score</div>
+  )
+}
+
